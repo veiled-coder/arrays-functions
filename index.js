9a24bcfbfed8c6090e0339adf05823d500a5e819
BintReina <omobowalerohma1998@gmail.com>
2023-03-04 12:11:48 +0100

    done 4 exercise

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
 
+/*
+1)Mutating array methods are methods that alters the original array
+while non-mutating array methods don't alter the original array,they return a new one
+
+examples under each category:
+mutating array methods:push(),pop(),unshift(),shift(),reverse()
+non-mutating methods:slice(),concat(),map(),filter(),spread operator{....}
+*/
+
+//-----------------question 2----------------
+
+let langs = ['C', 'JavaScript', 'Ruby', 'PHP', 'Python'];
+
+let addKotlin=langs.push('Kotlint')
+console.log(langs);
+
+let addjava = langs.splice(3, 0, 'Java');
+console.log(langs)
+
+let removeC = langs.shift();
+console.log(langs)
+
+let addScalaSwift = langs.unshift('Scala','Swift');
+console.log(langs)
+
+let replacePhp = langs[3] = 'Go Rust';
+console.log(langs)
+
+
+//-----------------question 3----------------
+
+let fruit = ['apple', 'mango', 'banana'];
+		function changeFruit( fruit ) {
+    			fruit[2] = "orange";
+    			return fruit;
+		}
+changeFruit(fruit);
+console.log(fruit);
+//fruit=['apple,'mango,'orange'] 
+
+//-----------------question 4----------------
+
+function myMax(my_array ) {
+    let maxResult = Math.max(...my_array);
+    console.log(maxResult)
+}
+
+let my_array = [2, 4, 5, 6, 10, 37];
+
+myMax(my_array);
 
 
 
 
 
 
-bb
-
-
+//--------------NOT PART OF ASSIGNMENT---------
 // console.log("hello world");
 // var arr = [5, 4, 3, 2, 1];
 // var poppedVal = arr.pop();
